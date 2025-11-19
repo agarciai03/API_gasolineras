f985d6420e1b36d500f5e39520a139b459b3cde7
Alberto García <albertogiz2005@gmail.com>
2025-11-19 22:55:37 +0100

    ❌Delete Archivos eliminados para cambiar la manera de enfocar el proyecto

--- utils/constants.js
+++
@@ -1,9 +0,0 @@
-export const urlProvincias = 'https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/Listados/Provincias/';
-
-export const urlMunicipios = 'https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/Listados/Municipios/';
-
-export const urlCombustibles = 'https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/Listados/ProductosPetroliferos/';
-
-export const urlGasolineras = 'https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroMunicipioProducto/';
-
-export const urlGasolinerasFechal = 'https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestresHist/FiltroMunicipioProducto/';
